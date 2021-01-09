4ddc1a3090a2cae2a64d4bdb990c212cfe7de17c
Natalia <nataliacaballero.d@gmail.com>
2021-01-09 00:42:05 -0300

    comit 1

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+   $(document).ready(function(){
+    
+    });
+        
+  
+      var mensaje=function(){
+         let name = $("#nameForm").val();
+         let email = $("#emailForm").val();
+         
+         swal({
+            title: "Nos hemos contactado",
+            text: 'Correo enviado exitosamente ' + name + ' a ' + email,
+            icon: "success",
+          });
+         $("#nameForm").val('');
+         $("#emailForm").val('');
+      }
+        
+
+  
+        
+       
+    
+    
+ 
+
+
+
+      
