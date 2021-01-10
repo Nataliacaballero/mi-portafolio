619ca6859ee13f78f1225dd38abfe1cc81e608bd
Natalia <nataliacaballero.d@gmail.com>
2021-01-09 23:43:31 -0300

    ventana modal y color formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,11 +7,21 @@
          let name = $("#nameForm").val();
          let email = $("#emailForm").val();
          
-         swal({
-            title: "Nos hemos contactado",
-            text: 'Correo enviado exitosamente ' + name + ' a ' + email,
-            icon: "success",
-          });
+         if(name.trim()!='' && email.trim()!=''){
+            swal({
+               title: "Nos hemos contactado",
+               text: 'Correo enviado exitosamente ' + name + ' a ' + email,
+               icon: "success",
+             });
+         }else{
+            swal({
+               title: "Por favor ingresa tus datos",
+               text: 'Por favor ingresa tus datos ',
+               icon: "error",
+             });
+         }
+
+       
          $("#nameForm").val('');
          $("#emailForm").val('');
       }
